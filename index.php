3411379f272590e25252537ef239864bc21efca7
DEXPRT <47742158+akmrpt@users.noreply.github.com>
2023-10-20 14:59:27 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>MSSQL Linq</title>
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <style>
+.r {
+    height: 100px;  
+    text-align: top;
+    
+  /* width: 25px; */
+  /* background-color: red; */
+  rotate: -90deg;
+}
+</style>
+</head>
+
+<body>
+    <div class="container" style="width: 800px;">
+
+        
+        <?php require('vw_sems.php') ; ?>
+
+        </div>
+        <!-- <div class="row">
+            <form id="form1" name="form1" method="post" action="action_page.php">
+                <div class="form-group">
+                    <label for="dd_prog">Programe:</label>
+                    <?php 
+                    //require_once('vw_prog.php') ; 
+                    ?>
+                </div>
+                <div class="form-group">
+                    <label for="dd_batch">Batch:</label>
+                    <?php 
+                    //require_once('vw_batch.php') ; 
+                    ?>
+                </div>
+                <div class="form-group">
+                    <label for="dd_semsNo">Semester No.:</label>
+                    <?php 
+                    //require_once('vw_semsNo.php') ; 
+                    ?>
+                </div>
+                <div class="form-group">
+                    
+                        <button type="submit" class="btn btn-default">Submit</button>
+                    
+                </div>
+            </form>
+        </div> -->
+    </div>
+</body>
+
+</html>
